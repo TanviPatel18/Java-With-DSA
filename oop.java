0d44c527603dc67897d899e1724231a83432c19c
TanviPatel18 <pateltanvi2528@gmail.com>
2026-05-28 16:47:19 +0530

    updated oop.java file

--- oop.java
+++ oop.java
@@ -1,3 +1,23 @@
+class Student {
+    private int age;
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
 public class oop {
-    
+  public static void main(String[] args) {
+
+        Student s1 = new Student();
+
+        s1.setAge(20);
+
+        System.out.println(s1.getAge());
+    }
 }
