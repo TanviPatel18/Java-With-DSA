2ff14704cbac1fa3476594a6cc2befd2c3c257e1
TanviPatel18 <pateltanvi2528@gmail.com>
2026-08-18 17:30:07 +0530

    start course's dsa practice

---
+++ 100x_dsa_prectic/loops_and_function.java
@@ -0,0 +1,27 @@
+// package 100x_dsa_prectic;
+  import java.util.Scanner;
+
+
+public class loops_and_function {
+ 
+     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = 1;
+        int end = sc.nextInt();
+
+        while (n <= end) 
+        {
+                     if(n%2==0)
+                     {
+                                          System.out.println(n);
+                     }
+                     
+            n++;
+        
+        }
+    }
+    }
+
+    
+
