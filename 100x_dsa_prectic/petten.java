7d87f2adee2bb842779432942e9c327dbbd5e5a5
TanviPatel18 <pateltanvi2528@gmail.com>
2026-09-10 12:35:32 +0530

    patten practice

---
+++ 100x_dsa_prectic/petten.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class petten {
+    
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        // int n = sc.nextInt();
+        
+
+       for (int i = 1; i <= 5; i++) {
+
+            for (int j = 1; j <= 5; j++) {
+
+                if (j >= 5 - i + 1) {
+                    System.out.print(" * ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+
+            System.out.println();
+        }
+        
+    }
+}
+
