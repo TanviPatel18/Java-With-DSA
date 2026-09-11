1012e42f3b76215e684658a0b922133bab250b2d
TanviPatel18 <pateltanvi2528@gmail.com>
2026-09-11 15:00:01 +0530

    some files

---
+++ YT/tempCodeRunnerFile.java
@@ -0,0 +1,15 @@
+public class recurtion {
+    public static void f(int i,int n)
+    {
+        if(i<n)
+        {
+          return;
+        }
+        System.out.println(i);
+        f(i-1,n);
+    }    
+    public static void main(String[] args)
+    {
+        f(5,1);
+    }
+}
