57a757c1ac46843e021b04ff5ebda3f8bd6ccc70
TanviPatel18 <pateltanvi2528@gmail.com>
2026-09-12 13:23:48 +0530

    dsa with youtube  learn hashing

---
+++ YT/hashing.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class hashing {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the size of Array: ");
+        int n=sc.nextInt();
+
+        
+        int[] arr=new int[n];
+        System.out.println();
+        for(int i=1;i<=arr.length;i++)
+        {
+            
+            System.out.print("Enter the " + (i) + " value of Array: ");
+            arr[i-1] = sc.nextInt();
+           
+        }
+
+        int[] hash = new int[100];
+
+        for(int i = 0; i < arr.length; i++) {
+            hash[arr[i]]++;
+        }
+        System.out.print("How many numbers you want to find: ");
+        int q = sc.nextInt();
+
+        int[] find = new int[q];
+        System.out.println();
+        for(int i = 0; i < q; i++) 
+        {
+            System.out.print("Enter number to find: ");
+            find[i] = sc.nextInt();
+        }
+        System.out.println();
+        for(int i = 0; i < q; i++) 
+        {
+            System.out.println(find[i] + " found " + hash[find[i]] + " times");
+        }
+    }
+}
